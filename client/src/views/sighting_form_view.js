ede542e901b161930a32cdce35eec6abf0c694aa
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-01-22 14:57:03 +0000

    initial files

---
+++ client/src/views/sighting_form_view.js
@@ -0,0 +1,23 @@
+const PubSub = require('../helpers/pub_sub.js')
+
+const SightingFormView = function (form) {
+  this.form = form;
+};
+
+SightingFormView.prototype.bindEvents = function () {
+  this.form.addEventListener('submit', (evt) => {
+    this.handleSubmit(evt);
+  });
+};
+
+SightingFormView.prototype.handleSubmit = function (evt) {
+  evt.preventDefault();
+}
+
+SightingFormView.prototype.createSighting = function (form) {
+  const newSighting = {
+
+  }
+};
+
+module.exports = SightingFormView;
